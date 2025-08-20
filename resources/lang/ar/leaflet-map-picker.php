43184b3ccde12020ba975122e978540967649b3a
GoodM4ven <goodm4ven@proton.me>
2025-08-20 14:28:49 +0300

    ...

---
+++ resources/lang/ar/leaflet-map-picker.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'selected_locations' => 'الموقع المحدد:',
+    'map_type' => 'نوع الخريطة',
+    'search_location' => 'ابحث عن موقع',
+    'search_placeholder' => 'ابحث عن موقع...',
+    'no_results' => 'لم يعثر على نتائج',
+    'cancel' => 'إلغاء',
+];
--- resources/lang/en/leaflet-map-picker.php
+++ resources/lang/en/leaflet-map-picker.php
@@ -1,6 +1,5 @@
 <?php
 
-// translations for Afsakar/LeafletMapPicker
 return [
     'selected_locations' => 'Selected Location:',
     'map_type' => 'Map Type',
