2dfaf8dadcb478127497db5667880abd57ecdfbc
GoodM4ven <goodm4ven@proton.me>
2025-08-21 14:15:57 +0300

    ...

--- resources/js/field.js
+++ resources/js/field.js
@@ -32,7 +32,7 @@ export default function mapTilerPicker({ config }) {
             hash: false,
             maxBounds: null,
             language: null,
-            geolocate: false
+            geolocate: false,
         },
 
         init() {
@@ -190,7 +190,9 @@ export default function mapTilerPicker({ config }) {
                     return this.container;
                 }
                 onRemove() {
-                    this.container?.parentNode?.removeChild(this.container);
+                    if (this.container && this.container.parentNode) {
+                        this.container.parentNode.removeChild(this.container);
+                    }
                     this.map = undefined;
                 }
             }
@@ -220,7 +222,9 @@ export default function mapTilerPicker({ config }) {
                     return this.container;
                 }
                 onRemove() {
-                    this.container?.parentNode?.removeChild(this.container);
+                    if (this.container && this.container.parentNode) {
+                        this.container.parentNode.removeChild(this.container);
+                    }
                     this.map = undefined;
                 }
             }
@@ -244,24 +248,26 @@ export default function mapTilerPicker({ config }) {
                     this.container.className = 'maplibregl-ctrl maplibregl-ctrl-group';
                     const btn = document.createElement('button');
                     btn.type = 'button';
-                    btn.innerHTML = `
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                        </svg>`;
+                    btn.innerHTML = [
+                        '<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">',
+                        '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />',
+                        '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />',
+                        '</svg>',
+                    ].join('');
                     btn.title = self.config.myLocationButtonLabel || 'My Location';
                     btn.onclick = () => self.goToCurrentLocation();
                     this.container.appendChild(btn);
                     return this.container;
                 }
                 onRemove() {
-                    this.container?.parentNode?.removeChild(this.container);
+                    if (this.container && this.container.parentNode) {
+                        this.container.parentNode.removeChild(this.container);
+                    }
                     this.map = undefined;
                 }
             }
             map.addControl(new LocationControl(), 'top-left');
         },
-
         goToCurrentLocation() {
             if (!navigator.geolocation) {
                 new FilamentNotification()
