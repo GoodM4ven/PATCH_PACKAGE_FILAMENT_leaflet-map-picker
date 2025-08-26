5ed913b919ec9ffb6c45eb2d3c0a2071cb156d29
GoodM4ven <goodm4ven@proton.me>
2025-08-26 17:29:30 +0300

    ...

--- resources/js/field.js
+++ resources/js/field.js
@@ -276,7 +276,7 @@ export default function mapTilerPicker({ config }) {
                         '<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35M11 19a8 8 0 100-16 8 8 0 000 16z"/></svg>';
                     btn.title = self.config.searchLocationButtonLabel || 'Search Location';
                     btn.onclick = () => {
-                        if (!self.lock.isLocked()) self.$dispatch('open-modal', { id: 'location-search-modal' });
+                        if (!_lock.isLocked()) self.$dispatch('open-modal', { id: 'location-search-modal' });
                     };
                     this.container.appendChild(btn);
                     return this.container;
